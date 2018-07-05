29e1361dec7932a7672dde3b41978f06c7b5825c
barry.paneer <barry.paneer@qq.com>
2018-07-05 18:45:51 +0800

    通达信1分钟线转换CSV功能基本完成

--- Report/TdxMin2CSV.py
+++ Report/TdxMin2CSV.py
@@ -21,8 +21,15 @@
    python3 -m pip install --upgrade numpy
    python3 -m pip install --upgrade pandas
 
+
 使用：
     通过python3调起脚本前，需要在[Configuration]章节中配置输入的通达信分钟线文件所在根目录
+
+    python3 TdxMin2CSV.py
+
+编写：    Barry
+日期：    2018/7/5
+
 
 """
 
@@ -38,7 +45,8 @@ from struct import *
 ### 必选参数
 # 通达信分钟线所在目录
 sTdxMinDataFolder = r"C:\zd_zszq\vipdoc\sh\minline"
-
+# 生成的CSV新格式数据文件的根目录
+sCSVFileFolder = r"./"
 
 ### 可选参数
 # 是否开启，预阅模式
@@ -82,8 +90,56 @@ def ConvertTDXFile( sTdxFilePath ):
     return lstMinData                # 返回1分钟线列表
 
 
+def Dispatch2CSVFiles( sCode, lstTdxMinRecords, sDispathFolder ):
+    """
+        将通达信的分钟线数据，根据市场、数据类型、代码、年份转存到对应的子目录和分文件中,如：
+            路径格式：  ./XXX/SSE(SZSE)/MIN/000001/MIN000001_2918.csv
+
+        sCode               商品代码
+        lstTdxMinRecords    通达信分钟线数据列表
+        sDispathFolder      数据分发转存文件根目录
+    """
+    sLastYear = ""                  # 记录上一次数据的年份
+    objCSVFile = None               # CSV文件句柄
+
+    ########### 循环遍历数据记录，按年份把分钟线数据转存到对应年份的文件中 ##########
+    for lstMinData in lstTdxMinRecords:
+        sDate = lstMinData[0]
+        sYear = sDate[:4]             # 当前数据记录的年份
+
+        ####### 根据年份判断是否需要新写一个文件（一个年份一个文件）#################
+        if "" == sLastYear or sLastYear != sYear:
+            sLastYear = sYear
+            sCSVFullFolder = os.path.join( sDispathFolder, sMarketCode + "/MIN/" + sCode )
+            sCSVFullFilePath = os.path.join( sCSVFullFolder, "MIN" + sCode + "_" + sYear + ".csv" )
+            if False == os.path.exists( sCSVFullFolder ):
+                os.makedirs( sCSVFullFolder )       # 递归创建目标目录(CSV)
+
+            if None != objCSVFile:                  # 关闭已经写过的文件
+                objCSVFile.close()
+
+            objCSVFile = open( sCSVFullFilePath, 'w' )
+            if None == sCSVFullFilePath:
+                raise Exception( "Dispatch2CSVFiles : 打开CSV目标文件打败 : {path}".format( path=sCSVFullFilePath )  )
+            ### 新建的CSV文件，先写入Min标题 ####
+            objCSVFile.write( "date,time,openpx,highpx,lowpx,closepx,settlepx,amount,volume,openinterest,numtrades,voip\n" )
+
+        ####### 将数据格式化后写入CSV文件中 #####################################3###
+        objCSVFile.write( "{date},{time},{openpx:.4f},{highpx:.4f},{lowpx:.4f},{closepx:.4f},0.0000,{amount:.4f},{volume},0,0,0.0000\n".format( date=lstMinData[0], time=lstMinData[1], openpx=lstMinData[2], highpx=lstMinData[3], lowpx=lstMinData[4], closepx=lstMinData[5], amount=lstMinData[6], volume=lstMinData[7] ) )
+
+    ########### 退出前关闭文件 ######################
+    if None != objCSVFile:
+        objCSVFile.close()
+
+
+
+
+
+
 ################# Entrance #############################################################
+        
 if __name__ == '__main__':
+
     sTdxMinDataFolder = sTdxMinDataFolder.replace( "\\", "/" )
     if False == os.path.exists( sTdxMinDataFolder ) :
         print( r"设定的通达信分钟线目录不存在：", sTdxMinDataFolder  )
@@ -98,14 +154,25 @@ if __name__ == '__main__':
             sys.exit( 1024 )
 
         ########## 遍历数据目录下的所有文件 & 转存 #################
+        nCount = 0
         lstTdxPath = os.listdir( sTdxMinDataFolder )
         for sTdxPath in lstTdxPath:
             if True == os.path.isdir( sTdxPath ):
                 continue
 
+            ### 读出每个文件中的1分钟线数据列表
             sTdxFullPath = os.path.join( sTdxMinDataFolder, sTdxPath )
-            lstTdxMinRecords = ConvertTDXFile( sTdxFullPath )   # 读出每个文件中的1分钟线数据列表
+            lstTdxMinRecords = ConvertTDXFile( sTdxFullPath )
 
+            sCode = sTdxPath[2:8] 
+            if int(sCode) <= 0:
+                print( "从tdx路径中提取到无效的商品代码", sCode, sTdxPath )
+                continue
+
+            ### 将每个通达信源文件中的数据按年份归类分别转存 #####
+            Dispatch2CSVFiles( sCode, lstTdxMinRecords, sCSVFileFolder )
+            print( str(nCount) + " [完成] 转储 ---> ", sTdxFullPath )
+            nCount += 1
 
 
 
